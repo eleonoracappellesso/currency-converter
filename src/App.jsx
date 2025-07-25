b0454cb8016ca4b80ec79fb3fb1711fdbce4e818
eleonoracappellesso <cappellessoeleonora@gmail.com>
2025-07-25 17:18:42 +0200

    scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import CurrencyInput from "./components/CurrencyInput";
+
+const API_BASE_URL = 'https://api.frankfurter.app';
